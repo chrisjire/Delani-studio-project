cb70f10a3eeb2d75729f6891676385446f35e591
chris <christianwilliamsjire@gmail.com>
2019-07-21 12:01:21 +0300

    This will add funtionality to the what we do icons using jQuery's hide and show methord

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+      $("#design").toggle();
+      $("#design2").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $("#design2").click(function() {
+      $("#design").toggle();
+      $("#design2").toggle();
+    });
+  });
+
+$(document).ready(function() {
+    $("#development").click(function() {
+      $("#development").toggle();
+      $("#development2").toggle();
+    });
+});
+$(document).ready(function() {
+    $("#development2").click(function() {
+      $("#development").toggle();
+      $("#development2").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#product").click(function() {
+      $("#product").toggle();
+      $("#product2").toggle();
+    });
+});
+$(document).ready(function() {
+    $("#product2").click(function() {
+      $("#product").toggle();
+      $("#product2").toggle();
+    });
+});
